743a4783b251a0b23223751470d065149ea1023c
naaaaeun <naeunlee1223@gmail.com>
2023-02-27 17:36:40 +0900

    0227

--- javaws/src/ws1/Ws.java
+++ javaws/src/ws1/Ws.java
@@ -1,13 +1,16 @@
 package ws1;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Ws {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		Random r =new Random();
+		
 		while (true) {
-			System.out.println("Input Command [q, a1, a2, a3, a4 ]");
+			System.out.println("Input Command [ q, a1, a2, a3, a4 ]");
 			String cmd = sc.next();
 
 			if (cmd.equals("q")) {
@@ -42,6 +45,29 @@ public class Ws {
 				// 1~9 까지의 랜덤한 숫자를 배열에 넣는다.
 				// 배열 정보를 출력한다.
 				// 배열의 합과 평균을 출력한다.
+				
+				System.out.println("Input first numbers [ 5 ~ 9 ] "); // 행값 입력받기
+				int fourth1=Integer.parseInt(sc.next()); // scanner로 입력받은 값 정수로 변환하여 저장
+				System.out.println("Input second numbers [ 5 ~ 9 ] "); // 열값 입력받기
+				int fourth2=Integer.parseInt(sc.next()); // scanner로 입력받은 값 정수로 변환하여 저장
+				
+				int[][] a4= new int[fourth1][fourth2]; //입력값에 따른 2차원 배열 생성
+				
+				int sum4=0; //합 저장할 변수 선언
+				double avg4=0; //평균 저장할 변수 선언
+				
+				for(int[] data1:a4) { //변수 행에 접근
+					for(int data2:data1) { //변수 행열에 접근하여 랜덤값 할당, 출력, 합산
+						data2=r.nextInt(9)+1;
+						System.out.printf("%d\t",data2);
+						sum4+=data2;
+					}
+					System.out.println("");
+				}
+				avg4=sum4/(fourth1*fourth2*1.0);
+				
+				System.out.printf("합: %d 평균:%.2f\n",sum4,avg4);
+
 			} else {
 				System.out.println("Invalid input.. try again");
 			}
